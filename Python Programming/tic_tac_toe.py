864a00198bdcd8cb4e1516b570b83eacbdb417ac
Ryan Hudson <xkcd@icloud.com>
2023-11-19 15:28:40 -0600

    Initial commit

---
+++ Python Programming/tic_tac_toe.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Author : Ryan Hudson
+Date   : 2023-11-19
+Purpose: Tic-Tac-Toe
+"""
+
+
+# --------------------------------------------------
+def main() -> None:
+    """Create a tic-tac-toe game"""
+    print("X or O?")
+
+
+# --------------------------------------------------
+if __name__ == "__main__":
+    main()
